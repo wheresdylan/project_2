e2a66ba9bca3024ea31df13cb305a28bc5c980b9
Dylan Marini <dylan.marini@g.austincc.edu>
2018-09-11 10:34:59 -0500

    added the models

---
+++ models/happyHour.js
@@ -0,0 +1,13 @@
+module.exports = function(sequelize, DataTypes) {
+    var HappyHour = sequelize.define("HappyHour", {
+        name: DataTypes.STRING,
+        address: DataTypes.STRING,
+        phone: DataTypes.STRING,
+        time: DataTypes.STRING,
+        deal: DataTypes.STRING,
+        image: DataTypes.STRING,
+        website: DataTypes.STRING,
+  
+    });
+    return HappyHour;
+  };
---
+++ models/uniqueDrinks.js
@@ -0,0 +1,12 @@
+
+module.exports = function(sequelize, DataTypes) {
+    var Unique = sequelize.define("Unique", {
+        name: DataTypes.STRING,
+        address: DataTypes.STRING,
+        phone: DataTypes.STRING,
+        image: DataTypes.STRING,
+        website: DataTypes.STRING,
+  
+    });
+    return unique;
+  };
